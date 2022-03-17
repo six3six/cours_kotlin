537dab7d668b21512dea502ac61e79e614a0ec7a
alphae-nix <18laurent@gmail.com>
2022-03-17 09:17:24 +0100

    modification de l'ordre :rocket:

--- app/src/main/java/fr/lololoulou/chucknorrisapp/Joke.kt
+++ app/src/main/java/fr/lololoulou/chucknorrisapp/Joke.kt
@@ -3,11 +3,11 @@ package fr.lololoulou.chucknorrisapp
 import java.util.*
 
 data class Joke(
-    val categories: List<String>,
     val createdAt: String,
     val iconUrl: String,
     val id: String,
     val updatedAt: String,
     val url : String,
-    val value : String
+    val value : String,
+    val categories: List<String> = listOf()
 )
