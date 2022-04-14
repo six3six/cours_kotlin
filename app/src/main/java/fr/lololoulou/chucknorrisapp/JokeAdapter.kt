221a35b4be41c88c3e8983883a52daac979778bd
alphae-nix <alphae-nix>
2022-04-14 12:40:02 +0200

    1.4 Custom view for items

--- app/src/main/java/fr/lololoulou/chucknorrisapp/JokeAdapter.kt
+++ app/src/main/java/fr/lololoulou/chucknorrisapp/JokeAdapter.kt
@@ -1,20 +1,29 @@
 package fr.lololoulou.chucknorrisapp
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
 class JokeAdapter: RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
     private val jokes = JokeList.getJokeList()
-    inner class JokeViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    inner class JokeViewHolder(val constraintLayout: ConstraintLayout) : RecyclerView.ViewHolder(constraintLayout){
+        fun updateJoke(text:String){
+            val constraintValue: TextView = constraintLayout.findViewById<TextView>(R.id.textView_joke_layout)
+            constraintValue.text = text
+        }
+    }
 
     //Create Joke View Holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
-        return JokeViewHolder(TextView(parent.context))
+        val testView: ConstraintLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.joke_layout,parent,false) as ConstraintLayout
+        return JokeViewHolder(testView)
     }
     //Replace the contents of the view
     override fun onBindViewHolder(holder: JokeAdapter.JokeViewHolder, position: Int) {
-        holder.textView.text = jokes[position].value
+        holder.updateJoke(jokes[position].value)
     }
     // Return the size of jokes dataset
     override fun getItemCount() = jokes.size
